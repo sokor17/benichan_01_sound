e9c85b49070b55cff4bff796caac43a8ed7931fd
YuKono <cosmos17128@gmail.com>
2021-05-12 03:53:25 +0900

    サウンドアプリチャレンジの完成

--- Sound/Sound/ViewController.swift
+++ Sound/Sound/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     
     //ドラムを表示する箱を作る
     @IBOutlet var drumButton:UIButton!
+    //ピアノを表示する箱を作る
+    @IBOutlet var pianoButton:UIButton!
+    //ギターを表示する箱を作る
+    @IBOutlet var guitarButton:UIButton!
     
-    // ファイル読み込み、プレイヤーの定義
+    // ドラムファイル読み込み、プレイヤーの定義
     let drumSoundPlayer = try!  AVAudioPlayer(data:NSDataAsset(name:"drumSound")!.data)
+    // ピアノファイル読み込み、プレイヤーの定義
+    let pianoSoundPlayer = try!  AVAudioPlayer(data:NSDataAsset(name:"pianoSound")!.data)
+    // ギターファイル読み込み、プレイヤーの定義
+    let guitarSoundPlayer = try!  AVAudioPlayer(data:NSDataAsset(name:"guitarSound")!.data)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,22 +32,52 @@ class ViewController: UIViewController {
     
     //ドラムのタッチが始まったあと
     @IBAction func touchDownDrumButton(){
-        
         //ドラムが鳴っている画像に切り替える
         drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         //音を最初戻す
         drumSoundPlayer.currentTime = 0
         //再生する
         drumSoundPlayer.play()
-        
     }
-    
     //ドラムのタッチ終了
     @IBAction func touchUpDrumButton(){
         //ドラムが鳴っていない画像に切り替える
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
         
     }
+    
+    //ピアノのタッチが始まったあと
+    @IBAction func touchDownPianoButton(){
+        //ピアノが鳴っている画像に切り替える
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        //音を最初戻す
+        pianoSoundPlayer.currentTime = 0
+        //再生する
+        pianoSoundPlayer.play()
+    }
+    //ピアノのタッチ終了
+    @IBAction func touchUpPianoButton(){
+        //ピアノが鳴っていない画像に切り替える
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+        
+    }
+    
+    //ギターのタッチが始まったあと
+    @IBAction func touchDownGuitarButton(){
+        //ギターが鳴っている画像に切り替える
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        //音を最初戻す
+        guitarSoundPlayer.currentTime = 0
+        //再生する
+        guitarSoundPlayer.play()
+    }
+    //ギターのタッチ終了
+    @IBAction func touchUpGuitarButton(){
+        //ギターが鳴っていない画像に切り替える
+        guitarButton.setImage(UIImage(named: "guitaeImage"), for: .normal)
+        
+    }
+    
 
 }
 
